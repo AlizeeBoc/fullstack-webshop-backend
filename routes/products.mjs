6c76be78a00965528ed60b8641aa3bafec781be2
Alizee <bocianalizee@gmail.com>
2023-08-22 14:52:01 +0200

    get all products, a specific product and create a product done

--- routes/products.mjs
+++ routes/products.mjs
@@ -1,32 +1,49 @@
-import express, {json} from "express"
+import express, { json } from "express"
 const router = express()
-import Product from "../Models/Product.mjs";
+import Product from "../Models/Product.mjs"
 
-router.use(json())
-
- 
-router.get('/', async (req, res) => {
-try {
+// GET all products
+router.get("/", async (req, res) => {
+  try {
     const products = await Product.find()
     res.json(products)
-} catch (err) {
-    console.error('Erreur lors de la récupération des produits :', err)
-    res.status(500).json({error: 'Server error'})
-}
+  } catch (err) {
+    console.error("Error while retrieving products :", err)
+    res.status(500).json({ error: "Server error" })
+  }
 })
 
-router.get('/:productRef', async (req, res) =>  {
-    try {
-        const productRef = req.params.productRef   
-        const product = await Product.find({productRef})
-        if(!product) {
-            res.status(404).json({error : "Product not find"})
-        }
-        res.json(product)
-    }catch (err) {
-        console.error('Error lors de la récupération du produit : ', err)
-        res.status(500).json({error : "Server Error"})
-    } 
+// Get a specific product
+router.get("/:reference", async (req, res) => {
+  try {
+    const reference = req.params.reference
+    const product = await Product.find({ reference })
+    if (!product) {
+      res.status(404).json({ error: "Product not find" })
+    }
+    res.json(product)
+  } catch (err) {
+    console.error("Error while retrieving the product:", err)
+    res.status(500).json({ error: "Server Error" })
+  }
 })
 
+// Add a product
+router.post("/add-product", async (req, res) => {
+  console.log("bonjour ")
+  try {
+    const product = new Product({
+      reference: req.body.reference,
+      description: req.body.description,
+      price: req.body.price,
+      //image : filename
+    })
+
+    const newProduct = await product.save()
+    res.send(newProduct)
+  } catch (err) {
+    console.error("Error creating the product", err)
+    res.status(500).send("Internal Server Error")
+  }
+})
 export default router
