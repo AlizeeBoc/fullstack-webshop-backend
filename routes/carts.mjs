e8d2bc8e5595fd9f2c31a624ba38f9f5fc063fc2
root <root@JL-laptop>
2023-08-24 12:57:34 +0200

    inserting cart items into database, and retrieving them

--- routes/carts.mjs
+++ routes/carts.mjs
@@ -1,15 +1,15 @@
-import express from "express";
-
-import Product from "../Models/Product.mjs";
-import session from "express-session";
-import cookieParser from "cookie-parser";
+import express from "express"
+import Cart from "../Models/Cart.mjs"
+import Product from "../Models/Product.mjs"
+import session from "express-session"
+import cookieParser from "cookie-parser"
 import { v4 as uuidv4 } from 'uuid'
 
 const router = express()
 const cart = {}
 
 //session and cookie-parser middleware
-router.use(cookieParser());
+router.use(cookieParser())
 router.use(session({
     secret: 'webshop1234',
     resave: false,
@@ -19,27 +19,28 @@ router.use(session({
 
 router.post('/addtocart/:productId', async (req, res) => {
     try {
-        let userId = req.session.userId
-
+        let userId = req.session.userId;
 
         if (!userId) {
-            userId = uuidv4()
-            req.session.userId = userId
+            userId = uuidv4();
+            req.session.userId = userId;
+            console.log('Setting userId in session:', userId);
         }
 
-        console.log('Generated userId:', userId)
-        const { productId } = req.params;
-        const { quantity, chest, waist, hips } = req.body;
+        console.log('Generated userId:', userId);
+        const { productId } = req.params
+        const { quantity, chest, waist, hips } = req.body
 
         // Fetch the product based on the productId
-        // const product = await Product.findById(productId);
+        // const product = await Product.findById(productId)
         
         //Create a cart if the user doesn't have one
         if(!cart[userId]) {
             cart[userId] = []
         }
 
-        const cartItem = {
+        const cartItem = new Cart({
+            userId: userId,
             reference: Product.reference,
             quantity,
             chest,
@@ -47,8 +48,9 @@ router.post('/addtocart/:productId', async (req, res) => {
             hips,
             price: Product.price,
             image: Product.image
-        };
+        })
         cart[userId].push(cartItem)
+        await cartItem.save()
         res.json({ msg: 'Item added to cart' })
     } catch (error) {
         console.error('Error adding item to cart:', error)
@@ -58,22 +60,25 @@ router.post('/addtocart/:productId', async (req, res) => {
 
 
 // retrieve cart items for the user
-router.get('/', (req, res) => {
+router.get('/',async(req, res) => {
     try {
-        const userId = req.session.userId
-        // cart items for the user
-        const cartItems = cart[userId] || [];
 
-        res.json(cartItems);
-        console.log(cartItems)
+        const userId = req.session.userId;
+        console.log('Retrieved userId from session:', userId);
+        
+        const cartItems = await Cart.find({ userId })
+
+        console.log('Fetched cart items:', cartItems)
+
+        res.json(cartItems)
     } catch (error) {
-        console.error('Error fetching cart:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        console.error('Error fetching cart:', error)
+        res.status(500).json({ message: 'Internal server error' })
     }
-});
+})
 
 
-export default router;
+export default router
 
 
 
