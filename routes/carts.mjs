4d2001818e5d94737376eb9b43119976825cd70b
root <root@JL-laptop>
2023-08-25 11:16:24 +0200

    adding checkout

--- routes/carts.mjs
+++ routes/carts.mjs
@@ -1,9 +1,11 @@
 import express from "express"
 import Cart from "../Models/Cart.mjs"
+import Order from "../Models/Order.mjs"
 import Product from "../Models/Product.mjs"
 import session from "express-session"
 import cookieParser from "cookie-parser"
 import { v4 as uuidv4 } from 'uuid'
+
 
 const router = express()
 const cart = {}
@@ -17,7 +19,7 @@ router.use(session({
     cookie: { secure: false }
 }))
 
-router.post('/addtocart/:productId', async (req, res) => {
+router.post('/order', async (req, res) => {
     try {
         let userId = req.session.userId;
 
@@ -50,7 +52,7 @@ router.post('/addtocart/:productId', async (req, res) => {
             image: Product.image
         })
         cart[userId].push(cartItem)
-        await cartItem.save()
+        // await cartItem.save()
         res.json({ msg: 'Item added to cart' })
     } catch (error) {
         console.error('Error adding item to cart:', error)
@@ -76,6 +78,49 @@ router.get('/',async(req, res) => {
         res.status(500).json({ message: 'Internal server error' })
     }
 })
+
+// Checkout
+router.post('/order/checkout', async (req, res) => {
+    try {
+        const userId = req.session.userId;
+        const cartItems = cart[userId] || [];
+        
+        if (cartItems.length === 0) {
+            return res.status(400).json({ message: 'Cart is empty' })
+        }
+
+        const { status, firstname, lastname, email, address, bankDetails } = req.body
+
+        const orderItems = cartItems.map(cartItem => ({
+            reference: cartItem.reference,
+            waist: cartItem.waist,
+            chest: cartItem.chest,
+            hips: cartItem.hips,
+            price: cartItem.price,
+            image: cartItem.image
+        }));
+
+        const order = new Order({
+            status,
+            firstname,
+            lastname,
+            email,
+            address,
+            bankDetails,
+            items: orderItems
+        });
+
+        await order.save()
+        // Clear the user's cart after checkout
+        cart[userId] = []
+        
+        res.status(201).json({ message: 'Order placed successfully' });
+    } catch (error) {
+        console.error('Error placing order:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
 
 
 export default router
--- routes/index.mjs
+++ routes/index.mjs
@@ -3,8 +3,8 @@ const router = express()
 
 import productRouter from "./products.mjs"
 router.use('/products', productRouter)
-//import cartRouter from "./carts.mjs"
-//router.use("/cart", cartRouter)
+import cartRouter from "./carts.mjs"
+router.use("/cart", cartRouter)
 import loginRouter from "./login.mjs"
 router.use('/login', loginRouter)
 
