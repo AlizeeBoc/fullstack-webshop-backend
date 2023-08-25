f76cae4bd4b48b49876e4d0f6a70d909536294de
Alizee <bocianalizee@gmail.com>
2023-08-25 13:31:10 +0200

    register & login controllers based on roles added

--- Models/User.mjs
+++ Models/User.mjs
@@ -10,11 +10,17 @@ const usersSchema = new mongoose.Schema({
      type: String,
      required: true,
     },
+    role: {
+      type : String,
+      enum : ["employee", "admin", "user"]
+    },
     password: {
      type: String,
      required: true,
-    }
-  })
+    },
+  },
+  //{ timeStamps : true }
+  )
   
   const User = mongoose.model("User", usersSchema)
   
